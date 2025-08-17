98c2cbe4b09b9baa43719f44830635926d36e67c
viklo <34488344+therealviklo@users.noreply.github.com>
2025-08-17 10:49:20 +0200

    switched library and made some progress

--- src/win.rs
+++ src/win.rs
@@ -1,5 +1,5 @@
-use winapi::{shared::{dxgi::{IDXGISwapChain, DXGI_SWAP_CHAIN_DESC, DXGI_SWAP_EFFECT_DISCARD}, dxgiformat::DXGI_FORMAT_B8G8R8A8_UNORM, dxgitype::{DXGI_MODE_DESC, DXGI_MODE_SCALING_UNSPECIFIED, DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED, DXGI_RATIONAL, DXGI_SAMPLE_DESC, DXGI_USAGE_RENDER_TARGET_OUTPUT}, windef::HWND}, um::{d3d11::{D3D11CreateDeviceAndSwapChain, ID3D11Device, ID3D11DeviceContext, D3D11_SDK_VERSION}, d3dcommon::D3D_DRIVER_TYPE_HARDWARE}};
-use std::ptr::{null, null_mut};
+use windows::{core::Interface, Win32::{Foundation::{HMODULE, HWND}, Graphics::{Direct3D::D3D_DRIVER_TYPE_HARDWARE, Direct3D11::{D3D11CreateDeviceAndSwapChain, ID3D11Device, ID3D11DeviceContext, D3D11_CREATE_DEVICE_FLAG, D3D11_SDK_VERSION}, Dxgi::{Common::{DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_MODE_DESC, DXGI_MODE_SCALING_UNSPECIFIED, DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED, DXGI_RATIONAL, DXGI_SAMPLE_DESC}, IDXGIAdapter, IDXGIDevice, IDXGIFactory, IDXGISwapChain, DXGI_MWA_NO_ALT_ENTER, DXGI_MWA_NO_PRINT_SCREEN, DXGI_MWA_NO_WINDOW_CHANGES, DXGI_SWAP_CHAIN_DESC, DXGI_SWAP_EFFECT_DISCARD, DXGI_USAGE_RENDER_TARGET_OUTPUT}}}};
+use std::ptr::null_mut;
 use crate::win_utils::*;
 
 pub struct Dvr {
@@ -32,25 +32,40 @@ impl Dvr {
 				Flags: 0,
 			};
 
-			// TODO: should be something like ComPtr
-			let mut swapchain: *mut IDXGISwapChain = null_mut();
-			let mut device: *mut ID3D11Device = null_mut();
-			let mut context: *mut ID3D11DeviceContext = null_mut();
+			let mut swapchain: Option<IDXGISwapChain> = None;
+			let mut device: Option<ID3D11Device> = None;
+			let mut context: Option<ID3D11DeviceContext> = None;
 
 			D3D11CreateDeviceAndSwapChain(
-				null_mut(),
+				None,
 				D3D_DRIVER_TYPE_HARDWARE,
-				null_mut(),
-				0,
-				null(),
-				0,
+				HMODULE(null_mut()),
+				D3D11_CREATE_DEVICE_FLAG(0),
+				None,
 				D3D11_SDK_VERSION,
-				&sd,
-				&mut swapchain,
-				&mut device,
-				null_mut(),
-				&mut context
-			).failed("Failed to initialise Direct3D 11")?;
+				Some(&sd),
+				Some(&mut swapchain),
+				Some(&mut device),
+				None,
+				Some(&mut context)
+			).map_err(|_| "Failed to initialise Direct3D 11")?;
+
+			{
+				let dxgi_device: IDXGIDevice =
+					device.ok_or("Device was not created")?.cast()
+					.map_err(|_| "Failed to get DXGI device")?;
+				
+				let dxgi_adapter: IDXGIAdapter =
+					dxgi_device.GetParent()
+					.map_err(|_| "Failed to get DXGI adapter")?;
+
+				let dxgi_factory: IDXGIFactory =
+					dxgi_adapter.GetParent()
+					.map_err(|_| "Failed to get DXGI factory")?;
+
+				dxgi_factory.MakeWindowAssociation(hwnd, DXGI_MWA_NO_ALT_ENTER | DXGI_MWA_NO_PRINT_SCREEN | DXGI_MWA_NO_WINDOW_CHANGES)
+					.map_err(|_| "Failed to make window associations")?;
+			}
 
 			Ok(Dvr {})
 		}
